e52a4f57ca0cd81ee1d8004a9eb6430f0a1ee2e7
Harran Ali <harran.m@gmail.com>
2021-03-17 10:16:12 +0400

    update example handler

--- httpd/handlers/home.go
+++ httpd/handlers/home.go
@@ -6,25 +6,19 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/harranali/gincoat/core"
-	"github.com/harranali/gincoat/models"
-	"gorm.io/gorm"
 )
 
 // HomeShow to show home page
 func HomeShow(c *gin.Context) {
-	db := c.MustGet(core.DB).(*gorm.DB)
-
-	db.First(&models.Product{})
 
 	c.JSON(200, gin.H{
-		"message": "show home page!",
+		"message": "hello from home show!",
 	})
 }
 
 // HomeUpdate to update home page
 func HomeUpdate(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"message": "update home page",
+		"message": "hello from home update",
 	})
 }
