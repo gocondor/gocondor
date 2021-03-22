74a79ab3ad19507d8fce3a3838dd4d0931a74b8f
Harran Ali <harran.m@gmail.com>
2021-03-22 15:12:42 +0400

    add test for set env

--- core/core_test.go
+++ core/core_test.go
@@ -2,9 +2,11 @@ package core_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	. "github.com/gincoat/gincoat/core"
+	"github.com/joho/godotenv"
 )
 
 func TestNew(t *testing.T) {
@@ -12,5 +14,18 @@ func TestNew(t *testing.T) {
 	typeOfApp := fmt.Sprintf("%T", app)
 	if typeOfApp != "*core.App" {
 		t.Errorf("expecting app varialbe to be of type *core.App")
+	}
+}
+
+func TestSetEnv(t *testing.T) {
+	env, err := godotenv.Read("./testingdata/.env")
+	if err != nil {
+		t.Errorf("failed reading .env file")
+	}
+	app := New()
+	app.SetEnv(env)
+
+	if os.Getenv("KEY_ONE") != "VAL_ONE" || os.Getenv("KEY_TWO") != "VAL_TWO" {
+		t.Errorf("failed to set env vars")
 	}
 }
