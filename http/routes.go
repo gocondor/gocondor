0333f4c4305af48f5d6efe48af90a6f495db6c0d
Harran Ali <harran.m@gmail.com>
2021-04-09 13:14:17 +0400

    rename httpd to http

--- http/routes.go
+++ http/routes.go
@@ -2,11 +2,11 @@
 // Use of this source code is governed by MIT-style
 // license that can be found in the LICENSE file.
 
-package httpd
+package http
 
 import (
 	"github.com/gocondor/core/routing"
-	"github.com/gocondor/gocondor/httpd/handlers"
+	"github.com/gocondor/gocondor/http/handlers"
 )
 
 // RegisterRoutes to register your routes
