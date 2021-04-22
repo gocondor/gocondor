0548f24ffec40e2f2db2d3336cacc019db2d8b80
Harran Ali <harran.m@gmail.com>
2021-04-22 22:52:30 +0400

    add code comments

--- http/input/input.go
+++ http/input/input.go
@@ -1,5 +1,6 @@
 package input
 
+// User represents request data with user information
 type User struct {
 	Name string `json:"name" binding:"exists,alphanum"`
 	Age  int    `json:"age" binding:"exists,alphanum,min=18"`
