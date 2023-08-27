9baded4db4eb383013e1a050bd251f64a41924b4
Harran Ali <harran.m@gmail.com>
2023-08-27 22:09:37 +0400

    implement reset password

--- handlers/authentication.go
+++ handlers/authentication.go
@@ -6,13 +6,17 @@ package handlers
 
 import (
 	"crypto/md5"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gocondor/core"
 	"github.com/gocondor/gocondor/events"
 	"github.com/gocondor/gocondor/models"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -92,19 +96,20 @@ func Signup(c *core.Context) *core.Response {
 	hashedCacheKey := c.CastToString(fmt.Sprintf("%x", md5.Sum([]byte(cacheKey))))
 	err = c.GetCache().Set(hashedCacheKey, token)
 	if err != nil {
+		c.GetLogger().Error(err.Error())
 		return c.Response.SetStatusCode(http.StatusInternalServerError).Json(c.MapToJson(map[string]interface{}{
-			"message": err.Error(),
+			"message": "internal server error",
 		}))
 	}
 
 	// fire user registered event
 	err = c.GetEventsManager().Fire(&core.Event{Name: events.USER_REGISTERED, Payload: map[string]interface{}{
-		"userStruct": user,
+		"user": user,
 	}})
 	if err != nil {
 		c.GetLogger().Error(err.Error())
 		return c.Response.SetStatusCode(http.StatusInternalServerError).Json(c.MapToJson(map[string]string{
-			"message": "internal error",
+			"message": "internal server error",
 		}))
 	}
 
@@ -128,6 +133,7 @@ func Signin(c *core.Context) *core.Response {
 	v := c.GetValidator().Validate(data, rules)
 
 	if v.Failed() {
+		c.GetLogger().Error(v.GetErrorMessagesJson())
 		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(v.GetErrorMessagesJson())
 	}
 
@@ -176,8 +182,9 @@ func Signin(c *core.Context) *core.Response {
 	hashedCacheKey := c.CastToString(fmt.Sprintf("%x", md5.Sum([]byte(cacheKey))))
 	err = c.GetCache().Set(hashedCacheKey, token)
 	if err != nil {
+		c.GetLogger().Error(err.Error())
 		return c.Response.SetStatusCode(http.StatusInternalServerError).Json(c.MapToJson(map[string]interface{}{
-			"message": err.Error(),
+			"message": "internal server error",
 		}))
 	}
 
@@ -186,7 +193,7 @@ func Signin(c *core.Context) *core.Response {
 	}))
 }
 
-func ResetPassword(c *core.Context) *core.Response {
+func ResetPasswordRequest(c *core.Context) *core.Response {
 	email := c.GetRequestParam("email")
 
 	// validation data
@@ -210,7 +217,7 @@ func ResetPassword(c *core.Context) *core.Response {
 	if res.Error != nil && !errors.Is(res.Error, gorm.ErrRecordNotFound) {
 		c.GetLogger().Error(res.Error.Error())
 		return c.Response.SetStatusCode(http.StatusInternalServerError).Json(c.MapToJson(map[string]string{
-			"message": "internal error",
+			"message": "internal server error",
 		}))
 	}
 	if res.Error != nil && errors.Is(res.Error, gorm.ErrRecordNotFound) {
@@ -219,20 +226,137 @@ func ResetPassword(c *core.Context) *core.Response {
 		}))
 	}
 
-	// TODO
-	// 1- handle the err
-	// 2- send reset password email
-	err := c.GetEventsManager().Fire(&core.Event{
-		Name: events.USER_PASSWORD_RESET_REQUESTED,
-		Payload: map[string]interface{}{
-			"userStruct": user,
-		},
-	})
+	// generate the link
+	expiresAt := time.Now().Add(time.Hour * 3).Unix()
+	linkCodeData := map[string]string{
+		"userID":    c.CastToString(user.ID),
+		"expiresAt": c.CastToString(expiresAt),
+	}
+	code := uuid.NewString()
+	c.GetCache().SetWithExpiration(code, c.MapToJson(linkCodeData), time.Hour*3)
+	err := c.GetEventsManager().Fire(&core.Event{Name: events.USER_PASSWORD_RESET_REQUESTED, Payload: map[string]interface{}{
+		"user": user,
+		"code": code,
+	}})
+	if err != nil {
+		c.GetLogger().Error(err.Error())
+		return c.Response.SetStatusCode(http.StatusInternalServerError).Json(c.MapToJson(map[string]string{
+			"message": "internal server error",
+		}))
+	}
 
-	return nil
+	return c.Response.Json(c.MapToJson(map[string]string{
+		"message": "reset password email sent successfully",
+	}))
 }
 
-// TODO implement
-func ChangePassword(c *core.Context) *core.Response {
-	return nil
+func SetNewPassword(c *core.Context) *core.Response {
+	urlCode := c.CastToString(c.GetPathParam("code"))
+	linkCodeDataStr, err := c.GetCache().Get(urlCode)
+	if err != nil {
+		c.GetLogger().Error(err.Error())
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "invalid link",
+		}))
+	}
+	var linkCode map[string]string
+	json.Unmarshal([]byte(linkCodeDataStr), &linkCode)
+	expiresAtUnix, err := strconv.ParseInt(linkCode["expiresAt"], 10, 64)
+	if err != nil {
+		c.GetLogger().Error(err.Error())
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "invalid link",
+		}))
+	}
+	expiresAt := time.Unix(expiresAtUnix, 0)
+	if time.Now().After(expiresAt) {
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "invalid link",
+		}))
+	}
+	userID, err := strconv.ParseUint(linkCode["userID"], 10, 64)
+	if err != nil {
+		c.GetLogger().Error(err.Error())
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "invalid link",
+		}))
+	}
+
+	oldPassword := c.CastToString(c.GetRequestParam("old_password"))
+	newPassword := c.CastToString(c.GetRequestParam("new_password"))
+	newPasswordConfirm := c.CastToString(c.GetRequestParam("new_password_confirm"))
+
+	// validation data
+	data := map[string]interface{}{
+		"old_password":         oldPassword,
+		"new_password":         newPassword,
+		"new_password_confirm": newPasswordConfirm,
+	}
+	// validation rules
+	rules := map[string]interface{}{
+		"old_password":         "required|length:6,10",
+		"new_password":         "required|length:6,10",
+		"new_password_confirm": "required|length:6,10",
+	}
+	// validate
+	v := c.GetValidator().Validate(data, rules)
+	if v.Failed() {
+		c.GetLogger().Error(v.GetErrorMessagesJson())
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(v.GetErrorMessagesJson())
+	}
+
+	var user models.User
+	res := c.GetGorm().Where("id = ?", userID).First(&user)
+	if res.Error != nil {
+		c.GetLogger().Error(res.Error.Error())
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "invalid link",
+		}))
+	}
+
+	SamePassword, err := c.GetHashing().CheckPasswordHash(user.Password, oldPassword)
+	if err != nil {
+		c.GetLogger().Error(err.Error())
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "invalid link",
+		}))
+	}
+
+	if !SamePassword {
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "the old password is incorrect",
+		}))
+	}
+
+	if newPassword != newPasswordConfirm {
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "new password does not match new password confirmation",
+		}))
+	}
+
+	hashedNewPassword, err := c.GetHashing().HashPassword(newPassword)
+	if err != nil {
+		c.GetLogger().Error(err.Error())
+		return c.Response.SetStatusCode(http.StatusUnprocessableEntity).Json(c.MapToJson(map[string]string{
+			"message": "invalid password",
+		}))
+	}
+
+	user.Password = hashedNewPassword
+	c.GetGorm().Save(&user)
+
+	err = c.GetEventsManager().Fire(&core.Event{Name: events.PASSWORD_CHANGED, Payload: map[string]interface{}{
+		"user": user,
+	}})
+
+	if err != nil {
+		c.GetLogger().Error(err.Error())
+		return c.Response.SetStatusCode(http.StatusInternalServerError).Json(c.MapToJson(map[string]string{
+			"message": "internal server error",
+		}))
+	}
+
+	return c.Response.Json(c.MapToJson(map[string]string{
+		"message": "password changed successfully",
+	}))
 }
