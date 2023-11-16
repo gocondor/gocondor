058050aaa869e755e870171f3078702345b2e252
Harran Ali <harran.m@gmail.com>
2023-11-16 12:53:33 +0400

    update home handler

--- handlers/home.go
+++ handlers/home.go
@@ -13,8 +13,3 @@ func WelcomeHome(c *core.Context) *core.Response {
 	message := "{\"message\": \"Welcome to GoCondor\"}"
 	return c.Response.Json(message)
 }
-
-func WelcomeToDashboard(c *core.Context) *core.Response {
-	message := "{\"message\": \"Welcome to Dashboard\"}"
-	return c.Response.Json(message)
-}
